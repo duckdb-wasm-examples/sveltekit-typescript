0b06b2b820770251e6d5776fe78684adca1c93a1
Jose Zavala <jose.m.zavala.gonzalez@gmail.com>
2022-07-17 00:20:57 -0400

    Updated SvelteKit and Vite dependencies

---
+++ vite.config.js
@@ -0,0 +1,45 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	// Consult https://github.com/sveltejs/svelte-preprocess
+// 	// for more information about preprocessors
+// 	preprocess: preprocess(),
+// 	kit: {
+// 		adapter: adapter({
+// 			pages: 'docs',
+// 			assets: 'docs',
+// 		}),
+// 		paths: {
+// 			base: '/sveltekit-typescript'
+// 		},
+// 		prerender: {
+// 			default: true
+// 		},
+// 		appDir: 'internal', // For github pages: https://www.npmjs.com/package/@sveltejs/adapter-static/v/next
+// 	}
+// };
+
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit()],
+	preprocess: preprocess(),
+	kit: {
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs',
+		}),
+		paths: {
+			base: '/sveltekit-typescript'
+		},
+		prerender: {
+			default: true
+		},
+		appDir: 'internal', // For github pages: https://www.npmjs.com/package/@sveltejs/adapter-static/v/next
+	}
+
+};
+
+export default config;
