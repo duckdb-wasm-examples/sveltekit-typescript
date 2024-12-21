fec480fa3753ef030f916bd742401ce5fac335dd
Ben Schmidt <bmschmidt@gmail.com>
2024-12-21 16:21:02 -0500

    bump to svelte 5

--- src/lib/duckdb.ts
+++ src/lib/duckdb.ts
@@ -1,24 +1,36 @@
-import * as duckdb from '@duckdb/duckdb-wasm';
-import duckdb_wasm from '/node_modules/@duckdb/duckdb-wasm/dist/duckdb-mvp.wasm?url';
-import duckdb_worker from '/node_modules/@duckdb/duckdb-wasm/dist/duckdb-browser-mvp.worker.js?worker';
-
-import type { AsyncDuckDB } from '@duckdb/duckdb-wasm';
+import duckdb_wasm from '@duckdb/duckdb-wasm/dist/duckdb-mvp.wasm?url';
+import mvp_worker from '@duckdb/duckdb-wasm/dist/duckdb-browser-mvp.worker.js?url';
+import duckdb_wasm_eh from '@duckdb/duckdb-wasm/dist/duckdb-eh.wasm?url';
+import eh_worker from '@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js?url';
+import type { AsyncDuckDB, DuckDBBundles } from '@duckdb/duckdb-wasm';
+import { browser } from '$app/environment';
 
 let db: AsyncDuckDB | null = null;
 
-const initDB = async () => {
-	if (db) {
-		return db; // Return existing database, if any
-	}
+export async function instantiateDuckDb(): Promise<AsyncDuckDB> {
 
-	// Instantiate worker
-	const logger = new duckdb.ConsoleLogger();
-	const worker = new duckdb_worker();
+  if (!browser) {
+    Error("Can only instantiate DuckDB from browser.")
+  }
 
-	// and asynchronous database
-	db = new duckdb.AsyncDuckDB(logger, worker);
-	await db.instantiate(duckdb_wasm);
-	return db;
-};
+  const duckdb = await import('@duckdb/duckdb-wasm');
 
-export { initDB }; // so we can import this elsewhere
+  const MANUAL_BUNDLES: DuckDBBundles = {
+    mvp: {
+      mainModule: duckdb_wasm,
+      mainWorker: mvp_worker,
+    },
+    eh: {
+      mainModule: duckdb_wasm_eh,
+      mainWorker: eh_worker,
+    },
+  };
+  // Select a bundle based on browser checks
+  const bundle = await duckdb.selectBundle(MANUAL_BUNDLES);
+  // Instantiate the asynchronus version of DuckDB-wasm
+  const worker = new Worker(bundle.mainWorker!);
+  const logger = new duckdb.ConsoleLogger();
+  const db = new duckdb.AsyncDuckDB(logger, worker);
+  await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
+  return db;
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,93 +1,80 @@
 <script lang="ts">
-  import { initDB } from '$lib/duckdb'
-  import Speech from '$lib/components/speech.svelte'
-  import Spinner from '$lib/components/Spinner.svelte'
-  export let data;
-  import { base } from '$app/paths';
-  async function load_db() {
-    console.log("LOADING DB")
-    // A simple case of a db of a single parquet file.
-    const db = await initDB();
-    await db.registerFileURL('SOTU.parquet', `${base}/SOTU.parquet`, 4, false);
-    const conn = await db.connect();
+	export let data;
+	import { instantiateDuckDb } from '$lib/duckdb';
+	import Speech from '$lib/components/speech.svelte';
+	import Spinner from '$lib/components/Spinner.svelte';
+	import { base } from '$app/paths';
 
-    console.log("PREPARING INSERT")
-    await conn.query(`CREATE TABLE p1 AS SELECT * FROM parquet_scan('SOTU.parquet')`);
-    await conn.query(`CREATE VIEW wordcounts_raw AS SELECT * FROM (SELECT "@id" id, 
-        UNNEST("nc:unigrams").word0 as word, 
-        UNNEST("nc:unigrams").count as count FROM
-        p1) t1
-    `);
-    await conn.query(`
-      CREATE TABLE wordcounts AS SELECT * FROM 
-      wordcounts_raw
-      NATURAL JOIN (SELECT word, SUM(count) as tot, COUNT(*) AS df FROM wordcounts_raw GROUP BY word) t2
-    `);
-    return conn;
-  }
+	async function load_db() {
+		console.log('LOADING DB');
+		// A simple case of a db of a single parquet file.
+		const db = await instantiateDuckDb();
+		await db.registerFileURL('SOTU.parquet', `${base}/SOTU.parquet`, 4, false);
+		const conn = await db.connect();
+		await conn.query(`CREATE TABLE p1 AS SELECT * FROM parquet_scan('SOTU.parquet')`);
+		return conn;
+	}
 
-  // Set up the db connection as an empty promise.
-  const conn_prom = load_db();
+	// Set up the db connection as an empty promise.
+	const conn_prom = load_db();
 
-  const years = []
-  // We could get this from the database, but no need to show off.
-  for (let y = 1934; y < 2021; y++) {
-    years.push(y)
-  }
+	const years = [];
+	// We could get this from the database, but no need to show off.
+	for (let y = 1934; y < 2021; y++) {
+		years.push(y);
+	}
 
+	$: results = new Promise(() => {});
 
-  $: results = new Promise(() => {})
-
-  async function get_year(y) {
-//    year = y;
-    const conn = await conn_prom;
-    results = conn.query(`
+	async function get_year(y) {
+		//    year = y;
+		const conn = await conn_prom;
+		results = conn.query(`
       SELECT * FROM parquet_scan('SOTU.parquet') WHERE year == ${y}
-    `)
-    const result = await results;
-    console.log({results, result})
-  }
+    `);
+		const result = await results;
+	}
 
-  // Start with FDR, because he's good.
-  conn_prom.then(() => get_year(1936))
+	// Start with FDR, because he's good.
+	conn_prom.then(() => get_year(1936));
 </script>
-
 
 <h1>Duck DB Svelte-kit demo</h1>
 
 <div class="description">
-  This is a demo of a static, database-driven page using duckdb WASM.
-  All the primary text is stored in a parquet file structured to the <a href="https://nonconsumptive.org">
-  nonconsumptive</a> text sharing spec. On load, it gets a copy of duckdb WASM and uses it to mount 
-  the parquet file into the database. Clicking on any year will load the state of the Union address for that year.
+	This is a demo of a static, database-driven page using duckdb WASM. All the primary text is stored
+	in a parquet file structured to the <a href="https://nonconsumptive.org"> nonconsumptive</a> text sharing
+	spec. On load, it gets a copy of duckdb WASM and uses it to mount the parquet file into the database.
+	Clicking on any year will load the state of the Union address for that year.
 </div>
 
 <div class="years">
-  {#each years as y}
-    <div class="year" on:click={() => get_year(y)}>{y}</div>
-  {/each}
+	{#each years as y}
+		<div class="year" on:click={() => get_year(y)}>{y}</div>
+	{/each}
 </div>
 {#await results}
-<Spinner></Spinner>
+	<Spinner></Spinner>
 {:then result}
-<Speech {results} />
+	<Speech {results} />
 {/await}
+
 <style>
-.years {
-  display: flex;
-  font-size: .75em;
-  flex-wrap: wrap;
-  flex-direction: row;
-  justify-content: center;
-  width: 90vw;
-}
-.year {
-  cursor: pointer;
-  padding: 0.1rem;
-  border: 1px solid black;
-  margin: 0.1rem;
-}
-.year:hover {
-  background: lightgray;
-}
+	.years {
+		display: flex;
+		font-size: 0.75em;
+		flex-wrap: wrap;
+		flex-direction: row;
+		justify-content: center;
+		width: 90vw;
+	}
+	.year {
+		cursor: pointer;
+		padding: 0.1rem;
+		border: 1px solid black;
+		margin: 0.1rem;
+	}
+	.year:hover {
+		background: lightgray;
+	}
 </style>
